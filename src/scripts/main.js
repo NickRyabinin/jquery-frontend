be74055ecb7b2ed717e8f9ab8cdcf48cb610fdd8
Николай <rnn2002@mail.ru>
2024-06-11 19:08:23 +0300

    Modify main.js - add some temporary code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 /** */
 import { buildTable } from "./builder.js";
+import { createBook, readBook, updateBook, deleteBook } from "./book.js";
+
+$(document).ready(function() {
+  $('.books').click(function() {
+    $('.submenu').slideToggle();
+  });
+
+  $('.submenu li').click(function() {
+    const action = $(this).text();
+    switch(action) {
+      case 'Create':
+        createBook();
+        break;
+      case 'Read':
+        readBook();
+        break;
+      case 'Update':
+        updateBook();
+        break;
+      case 'Delete':
+        deleteBook();
+        break;
+      default:
+        break;
+    }
+  });
+});
 
 const data = [
   {"id":1,"login":"Ur-Quan Master","created_at":"2024-01-22 16:41:09"},

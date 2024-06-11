2504478dfa2f76f31be5293fe01abea30642fe2f
Николай <rnn2002@mail.ru>
2024-06-11 19:07:21 +0300

    Modify book.js - add templates for main functions

---
+++ src/scripts/book.js
@@ -0,0 +1,17 @@
+function createBook() {
+  alert('Create function called');
+}
+
+function readBook() {
+  alert('Read function called');
+}
+
+function updateBook() {
+  alert('Update function called');
+}
+
+function deleteBook() {
+  alert('Delete function called');
+}
+
+export { createBook, readBook, updateBook, deleteBook };

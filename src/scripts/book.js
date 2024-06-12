2510b5335c45319c7b535a2bbd5e553beace4fa0
Николай <rnn2002@mail.ru>
2024-06-12 18:40:26 +0300

    Add  GET /books/ jQuery Ajax request

--- src/scripts/book.js
+++ src/scripts/book.js
@@ -1,9 +1,16 @@
+import { buildTable } from "./builder.js";
+
 function createBook() {
   alert('Create function called');
 }
 
 function readBook() {
-  alert('Read function called');
+  $.get("http://php-crud-api.alwaysdata.net/books/", function(data, status){
+    if (status === 'success') {
+      const tableElement = buildTable(data);
+      $('main').append(tableElement);
+    }
+  });
 }
 
 function updateBook() {
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,4 @@
 /** */
-import { buildTable } from "./builder.js";
 import { createBook, readBook, updateBook, deleteBook } from "./book.js";
 
 $(document).ready(function() {
@@ -27,13 +26,3 @@ $(document).ready(function() {
     }
   });
 });
-
-const data = [
-  {"id":1,"login":"Ur-Quan Master","created_at":"2024-01-22 16:41:09"},
-  {"id":3,"login":"Mur-Mur","created_at":"2024-01-22 16:42:24"},
-  {"id":6,"login":"MurQuan","created_at":"2024-01-24 11:44:17"},
-  {"id":7,"login":"Mirelurk","created_at":"2024-01-31 17:19:00"}
-];
-
-const tableElement = buildTable(data);
-$('main').append(tableElement);

f8c00b63a8753dbf1a33177eb55ad97add4dffb5
Николай <rnn2002@mail.ru>
2024-06-13 18:10:03 +0300

    Modify main.js - add users menu handler

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,37 @@
 /** */
+import { createUser, readUser, updateUser, deleteUser } from "./user.js";
 import { createBook, readBook, updateBook, deleteBook } from "./book.js";
 
 $(document).ready(function() {
-  $('.books').click(function() {
-    $('.submenu').slideToggle();
+  $('.users').click(function() {
+    $('.booksSubmenu').slideUp();
+    $('.usersSubmenu').slideToggle();
   });
 
-  $('.submenu li').click(function() {
+  $('.books').click(function() {
+    $('.usersSubmenu').slideUp();
+    $('.booksSubmenu').slideToggle();
+  });
+
+  $('.usersSubmenu li').click(function() {
+    const action = $(this).text();
+    switch(action) {
+      case 'Create':
+        createUser();
+        break;
+      case 'Read':
+        readUser();
+        break;
+      case 'Update':
+        updateUser();
+        break;
+      case 'Delete':
+        deleteUser();
+        break;
+    }
+  });
+
+  $('.booksSubmenu li').click(function() {
     const action = $(this).text();
     switch(action) {
       case 'Create':
@@ -20,8 +45,6 @@ $(document).ready(function() {
         break;
       case 'Delete':
         deleteBook();
-        break;
-      default:
         break;
     }
   });

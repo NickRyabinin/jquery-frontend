5266c8fbad05f9f74411f7b7889f9479feda70c8
Николай <rnn2002@mail.ru>
2024-06-15 17:52:49 +0300

    Modify read() function - add click on "id" action

--- src/scripts/book.js
+++ src/scripts/book.js
@@ -4,11 +4,19 @@ function createBook() {
   alert('Create function called');
 }
 
-function readBook() {
-  $.get("http://php-crud-api.alwaysdata.net/books/", function(data, status){
+function readBook(id = "") {
+  $.get("http://php-crud-api.alwaysdata.net/books/" + id, function(data, status){
     if (status === 'success') {
       const tableElement = buildTable(data);
       $('main').append(tableElement);
+
+      $('td').click(function() {
+        if ($(this).index() === $('th:contains("id")').index()) {
+          if ($(this).text()) {
+            readBook($(this).text());
+          }
+        }
+      });
     }
   });
 }
--- src/scripts/user.js
+++ src/scripts/user.js
@@ -4,11 +4,19 @@ function createUser() {
   alert('Create function called');
 }
 
-function readUser() {
-  $.get("http://php-crud-api.alwaysdata.net/users/", function(data, status){
+function readUser(id = "") {
+  $.get("http://php-crud-api.alwaysdata.net/users/" + id, function(data, status){
     if (status === 'success') {
       const tableElement = buildTable(data);
       $('main').append(tableElement);
+
+      $('td').click(function() {
+        if ($(this).index() === $('th:contains("id")').index()) {
+          if ($(this).text()) {
+            readUser($(this).text());
+          }
+        }
+      });
     }
   });
 }

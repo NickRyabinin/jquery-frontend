a7611fb25a293adcfce53480e17dd7ffe79d2de2
Николай <rnn2002@mail.ru>
2024-06-16 12:42:38 +0300

    Modify user.js - add createUser() function

--- src/scripts/user.js
+++ src/scripts/user.js
@@ -1,16 +1,37 @@
-import { buildTable } from "./builder.js";
+import { apiUrl } from "./main.js";
+import { buildTable, buildForm } from "./builder.js";
 
 function createUser() {
-  alert('Create function called');
+  const fillableProperties = ["login", "email"];
+  const form = buildForm(fillableProperties);
+  $('main').append(form);
+
+  form.submit(function (event) {
+    event.preventDefault();
+
+    const formData = JSON.stringify({
+      "login": $('#login').val(),
+      "email": $('#email').val()
+    });
+
+    $.post(apiUrl + "users/", formData)
+      .done(function (responseData) {
+        alert("Token: " + responseData.token);
+      })
+      .fail(function (jqXHR) {
+        const errorResponse = JSON.parse(jqXHR.responseText);
+        alert("Ошибка: " + errorResponse.error);
+      });
+  });
 }
 
 function readUser(id = "") {
-  $.get("http://php-crud-api.alwaysdata.net/users/" + id, function(data, status){
+  $.get(apiUrl + "users/" + id, function (data, status) {
     if (status === 'success') {
       const tableElement = buildTable(data);
       $('main').append(tableElement);
 
-      $('td').click(function() {
+      $('td').click(function () {
         if ($(this).index() === $('th:contains("id")').index()) {
           if ($(this).text()) {
             readUser($(this).text());

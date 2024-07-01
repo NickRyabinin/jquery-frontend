a26cfd3d45e8a226dd9970c49257f24ebe334cc3
Николай <rnn2002@mail.ru>
2024-07-01 12:28:39 +0300

    Add opinion.js template

---
+++ src/scripts/opinion.js
@@ -0,0 +1,8 @@
+import { apiUrl } from "./main.js";
+import { buildTable, buildForm, buildPaginationButtons, setPaginationActions } from "./builder.js";
+
+function readOpinion(id = "", page = 1) {
+  alert("Read() function invoked");
+}
+
+export { readOpinion };

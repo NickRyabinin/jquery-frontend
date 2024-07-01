5cf5ea27cdc23adac3d24dd4e003dae84ec4a7fe
Николай <rnn2002@mail.ru>
2024-07-01 12:25:10 +0300

    Modify main.js - add actions for "Opinions" submenu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,25 +2,35 @@
 import { getHomePage } from "./home.js";
 import { authorizeUser, createUser, readUser, deleteUser } from "./user.js";
 import { createBook, readBook, updateBook, deleteBook } from "./book.js";
+import { readOpinion } from "./opinion.js";
 
 const apiUrl = "http://php-crud-api.alwaysdata.net/";
 
 $(document).ready(function() {
+  $('#home').click(function() {
+    $('.usersSubmenu').slideUp();
+    $('.booksSubmenu').slideUp();
+    $('.opinionsSubmenu').slideUp();
+    getHomePage();
+  })
+
   $('.users').click(function() {
     $('.booksSubmenu').slideUp();
+    $('.opinionsSubmenu').slideUp();
     $('.usersSubmenu').slideToggle();
   });
 
   $('.books').click(function() {
     $('.usersSubmenu').slideUp();
+    $('.opinionsSubmenu').slideUp();
     $('.booksSubmenu').slideToggle();
   });
 
-  $('#home').click(function() {
-    $('.booksSubmenu').slideUp();
+  $('.opinions').click(function() {
     $('.usersSubmenu').slideUp();
-    getHomePage();
-  })
+    $('.booksSubmenu').slideUp();
+    $('.opinionsSubmenu').slideToggle();
+  });
 
   $('.usersSubmenu li').click(function() {
     const action = $(this).text();
@@ -56,6 +66,24 @@ $(document).ready(function() {
         break;
       case 'Delete':
         deleteBook();
+        break;
+    }
+  });
+
+  $('.opinionsSubmenu li').click(function() {
+    const action = $(this).text();
+    switch(action) {
+      case 'Create':
+        createOpinion();
+        break;
+      case 'Read':
+        readOpinion();
+        break;
+      case 'Update':
+        updateOpinion();
+        break;
+      case 'Delete':
+        deleteOpinion();
         break;
     }
   });

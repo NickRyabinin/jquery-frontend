ddf70e76f9a14ea036735f6b494dd5ff8b6d8621
Николай <rnn2002@mail.ru>
2024-07-07 11:07:57 +0300

    Add view.js for showing content to user

---
+++ src/scripts/view.js
@@ -0,0 +1,34 @@
+import { apiUrl } from "./main.js";
+
+function getHomePage() {
+  $.get(apiUrl, function(data, status) {
+    clearContent();
+
+    if (status === 'success') {
+      const parsedData = extractBodyContent(data);
+      $('section').html(parsedData);
+    }
+  });
+}
+
+function extractBodyContent(data) {
+  const startIndex = data.indexOf('<body');
+  const endIndex = data.indexOf('</body>');
+
+  if (startIndex !== -1 && endIndex !== -1) {
+    const bodyContent = data.substring(startIndex, endIndex + '</body>'.length);
+    return bodyContent;
+  }
+
+  return "";
+}
+
+function showMessage(message) {
+  return $('#message').html(message);
+}
+
+function clearContent() {
+  $('section, form, table, #pagination-buttons').empty();
+}
+
+export { getHomePage, showMessage, clearContent };

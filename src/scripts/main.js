0d271e557a4054f0237253c80198570302dcbbb9
Николай <rnn2002@mail.ru>
2024-07-09 12:39:15 +0300

    Modify main.js - change setup method of "click" actions on menu items

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,92 +1,51 @@
 /** */
 import { getHomePage } from "./view.js";
-import { authorizeUser, createUser, readUser, deleteUser } from "./user.js";
+import { authorizeUser, createUser, readUser, updateUser, deleteUser } from "./user.js";
 import { createBook, readBook, updateBook, deleteBook } from "./book.js";
 import { createOpinion, readOpinion, updateOpinion, deleteOpinion } from "./opinion.js";
 
 const apiUrl = "http://php-crud-api.alwaysdata.net/";
 
-$(document).ready(function() {
-  $('#home').click(function() {
+$(document).ready(function () {
+  $('#home').click(function () {
     $('.usersSubmenu').slideUp();
     $('.booksSubmenu').slideUp();
     $('.opinionsSubmenu').slideUp();
     getHomePage();
   })
 
-  $('.users').click(function() {
+  $('.users').click(function () {
     $('.booksSubmenu').slideUp();
     $('.opinionsSubmenu').slideUp();
     $('.usersSubmenu').slideToggle();
   });
 
-  $('.books').click(function() {
+  $('.books').click(function () {
     $('.usersSubmenu').slideUp();
     $('.opinionsSubmenu').slideUp();
     $('.booksSubmenu').slideToggle();
   });
 
-  $('.opinions').click(function() {
+  $('.opinions').click(function () {
     $('.usersSubmenu').slideUp();
     $('.booksSubmenu').slideUp();
     $('.opinionsSubmenu').slideToggle();
   });
 
-  $('.usersSubmenu li').click(function() {
+  $('.submenu li').click(function () {
     const action = $(this).text();
-    switch(action) {
-      case 'Authorize':
-        authorizeUser();
-        break;
-      case 'Create':
-        createUser();
-        break;
-      case 'Read':
-        readUser();
-        break;
-      case 'Update':
-        break;
-      case 'Delete':
-        deleteUser();
-        break;
+    const menuName = $(this).closest('ul').parent().text().split(" ")[0]; // Получаем текст родительского элемента li
+    const functionName = action.toLowerCase() + menuName.slice(0, -2);
+
+    const functionCall = functionName + '()';
+    
+    try {
+        eval(functionCall);
+    } catch (error) {
+        console.error('Error calling function: ' + error);
     }
   });
 
-  $('.booksSubmenu li').click(function() {
-    const action = $(this).text();
-    switch(action) {
-      case 'Create':
-        createBook();
-        break;
-      case 'Read':
-        readBook();
-        break;
-      case 'Update':
-        updateBook();
-        break;
-      case 'Delete':
-        deleteBook();
-        break;
-    }
-  });
-
-  $('.opinionsSubmenu li').click(function() {
-    const action = $(this).text();
-    switch(action) {
-      case 'Create':
-        createOpinion();
-        break;
-      case 'Read':
-        readOpinion();
-        break;
-      case 'Update':
-        updateOpinion();
-        break;
-      case 'Delete':
-        deleteOpinion();
-        break;
-    }
-  });
 });
 
 getHomePage();

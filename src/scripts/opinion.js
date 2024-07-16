2b9b0d97e6a0ab862338b6a730aaa547a7adbc01
Николай <rnn2002@mail.ru>
2024-07-16 12:48:02 +0300

    Modify opinion.js - fix syntax error

--- src/scripts/opinion.js
+++ src/scripts/opinion.js
@@ -50,33 +50,33 @@ function readOpinion(bookId = "", page = 1, opinionId = "") {
       }
     });
   }
+}
 
-  function getOpinionData(bookId, page, opinionId) {
-    let query = (opinionId === "") ? '?page=' + page : "";
-    $.get(apiUrl + "books/" + bookId + "/opinions/" + opinionId + query)
-      .done(function (rawData) {
-        let data = (opinionId === "") ? rawData['items'] : rawData;
-        const tableElement = buildTable(data);
-        $('main').append(tableElement);
-        if (opinionId === "") {
-          const paginationButtons = buildPaginationButtons();
-          $('main').append(paginationButtons);
-          setPaginationActions(readOpinion, page, bookId);
-        }
-        $('td').click(function () {
-          if ($(this).index() === $('th:contains("opinion_id")').index()) {
-            if ($(this).text()) {
-              readOpinion(bookId, "", $(this).text());
-            }
+function getOpinionData(bookId, page, opinionId) {
+  let query = (opinionId === "") ? '?page=' + page : "";
+  $.get(apiUrl + "books/" + bookId + "/opinions/" + opinionId + query)
+    .done(function (rawData) {
+      let data = (opinionId === "") ? rawData['items'] : rawData;
+      const tableElement = buildTable(data);
+      $('main').append(tableElement);
+      if (opinionId === "") {
+        const paginationButtons = buildPaginationButtons();
+        $('main').append(paginationButtons);
+        setPaginationActions(readOpinion, page, bookId);
+      }
+      $('td').click(function () {
+        if ($(this).index() === $('th:contains("opinion_id")').index()) {
+          if ($(this).text()) {
+            readOpinion(bookId, "", $(this).text());
           }
-        });
-      })
-      .fail(function (jqXHR) {
-        clearContent();
-        const errorResponse = JSON.parse(jqXHR.responseText);
-        showMessage(errorResponse);
+        }
       });
-  }
+    })
+    .fail(function (jqXHR) {
+      clearContent();
+      const errorResponse = JSON.parse(jqXHR.responseText);
+      showMessage(errorResponse);
+    });
 }
 
 function updateOpinion() {
